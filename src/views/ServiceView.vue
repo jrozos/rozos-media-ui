f09bfae9e023d7493f6479a9a39ea015a0a2bceb
jrozos <sonarozos@gmail.com>
2024-05-20 13:46:12 -0600

    Front service with assets

--- src/views/ServiceView.vue
+++ src/views/ServiceView.vue
@@ -4,79 +4,26 @@
       <h2 class="display-5 fw-bold lh-1 mb-3">Servicios</h2>
     </div>
   </div>
-  <div class="row flex-lg-row align-items-center g-5 py-5">
+  <div
+    v-for="service in services"
+    class="row flex-lg-row align-items-center g-5 py-5"
+  >
     <div class="col-10 col-sm-8 col-lg-6">
-      <img
-        src="http://rozos-media.me/img/hero-image.jpeg"
-        class="d-block mx-lg-auto img-fluid rounded shadow"
-        alt="Bootstrap Themes"
-        width="700"
-        height="500"
-        loading="lazy"
-      />
-    </div>
-    <div class="col-lg-6">
-      <h1 class="display-5 fw-bold lh-1 mb-3">Rozos Media</h1>
-      <p class="lead">
-        9 + years of experience as a Web Developer adept in all stages of
-        advanced web development. Hands-on experience in user interface,
-        testing, and debugging processes. Experienced in Laravel, Bootstrap,
-        AEM, and Vue. Able to effectively self-manage during independent
-        projects.
-      </p>
-      <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-        <button type="button" class="btn btn-dark btn-lg px-4 me-md-2">
-          Contacto
-        </button>
+      <div v-for="asset in service.assets" :key="asset.id">
+        <img
+          :src="host + asset.image"
+          :alt="asset.name"
+          class="d-block mx-lg-auto img-fluid rounded shadow"
+          width="700"
+          height="500"
+          loading="lazy"
+        />
       </div>
     </div>
-  </div>
-  <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-    <div class="col-10 col-sm-8 col-lg-6">
-      <img
-        src="http://rozos-media.me/img/hero-image.jpeg"
-        class="d-block mx-lg-auto img-fluid rounded shadow"
-        alt="Bootstrap Themes"
-        width="700"
-        height="500"
-        loading="lazy"
-      />
-    </div>
     <div class="col-lg-6">
-      <h1 class="display-5 fw-bold lh-1 mb-3">Rozos Media</h1>
+      <h1 class="display-5 fw-bold lh-1 mb-3">{{ service.name }}</h1>
       <p class="lead">
-        9 + years of experience as a Web Developer adept in all stages of
-        advanced web development. Hands-on experience in user interface,
-        testing, and debugging processes. Experienced in Laravel, Bootstrap,
-        AEM, and Vue. Able to effectively self-manage during independent
-        projects.
-      </p>
-      <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-        <button type="button" class="btn btn-dark btn-lg px-4 me-md-2">
-          Contacto
-        </button>
-      </div>
-    </div>
-  </div>
-  <div class="row flex-lg-row align-items-center g-5 py-5">
-    <div class="col-10 col-sm-8 col-lg-6">
-      <img
-        src="http://rozos-media.me/img/hero-image.jpeg"
-        class="d-block mx-lg-auto img-fluid rounded shadow"
-        alt="Bootstrap Themes"
-        width="700"
-        height="500"
-        loading="lazy"
-      />
-    </div>
-    <div class="col-lg-6">
-      <h1 class="display-5 fw-bold lh-1 mb-3">Rozos Media</h1>
-      <p class="lead">
-        9 + years of experience as a Web Developer adept in all stages of
-        advanced web development. Hands-on experience in user interface,
-        testing, and debugging processes. Experienced in Laravel, Bootstrap,
-        AEM, and Vue. Able to effectively self-manage during independent
-        projects.
+        {{ service.description }}
       </p>
       <div class="d-grid gap-2 d-md-flex justify-content-md-start">
         <button type="button" class="btn btn-dark btn-lg px-4 me-md-2">
@@ -87,7 +34,28 @@
   </div>
 </template>
 <script>
+import apiClient from "@/axios.js";
 export default {
   name: "ServiceView",
+
+  data() {
+    return {
+      services: [],
+      host: "http://rozos-media.me/storage/",
+    };
+  },
+  created() {
+    this.fetchServices();
+  },
+  methods: {
+    async fetchServices() {
+      try {
+        const response = await apiClient.get("/services");
+        this.services = response.data;
+      } catch (error) {
+        console.error("Error fetching services:", error);
+      }
+    },
+  },
 };
 </script>
