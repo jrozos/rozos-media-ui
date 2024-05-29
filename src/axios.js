003e0c0b2ca7db4764fbb4ca612ad384ec5920e5
jrozos <sonarozos@gmail.com>
2024-05-28 21:50:35 -0600

    REAL API URL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://rozos-media.me/api", // Laravel API base URL
+  // baseURL: "https://rozos-media.me/api", // Laravel API base URL
+  baseURL: "https://app.rozosmedia.com/api", // Laravel API base URL
   headers: {
     "Content-Type": "application/json",
   },
